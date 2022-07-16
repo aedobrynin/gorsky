c500a031dc49a51443c1d8d009f6d9e9c5eb6491
aedobrynin <hashlib@yandex.ru>
2022-07-16 15:34:56 +0300

    Util package added

--- cmd/root.go
+++ cmd/root.go
@@ -6,7 +6,9 @@ package cmd
 
 import (
 	"os"
+    "fmt"
 
+    "github.com/aedobrynin/gorsky/util"
 	"github.com/spf13/cobra"
 )
 
@@ -15,16 +17,16 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gorsky",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "The program makes colored photo from S.M. Prokudin-Gorsky's negatives.",
+	Long: "The program makes colored photo from S.M. Prokudin-Gorsky's negatives.",
+    CompletionOptions: cobra.CompletionOptions{
+        DisableDefaultCmd: true,
+    },
+    Args: cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+        fmt.Println("root called")
+        util.JustWorks()
+    },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -45,7 +47,6 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 
