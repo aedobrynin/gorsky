80d9b7515e290b960b92b71849c4843823d06aac
aedobrynin <hashlib@yandex.ru>
2022-07-17 15:49:11 +0300

    Use image.Gray16 where it's possible

--- util/utils.go
+++ util/utils.go
@@ -1,79 +1,85 @@
 package util
 
 import (
-	"errors"
-	"fmt"
-	"image"
-	"image/jpeg"
-	"image/png"
+    "errors"
+    "fmt"
+    "image"
+    "image/jpeg"
+     "image/png"
     "image/color"
     "golang.org/x/image/draw"
-	"golang.org/x/image/tiff"
-	"os"
-	"path/filepath"
-	"sync"
-	"sync/atomic"
+    "golang.org/x/image/tiff"
+    "os"
+    "path/filepath"
+    "sync"
+    "sync/atomic"
 )
 
 func ProcessImages(paths []string, resultDirPath string) error {
-	resultDirAbsPath, err := createDir(resultDirPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while creating result directory %v: %v\n", resultDirPath, err)
-		return errors.New("Some files weren't processed, check stderr for more information")
-	}
+    resultDirAbsPath, err := createDir(resultDirPath)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error while creating result directory %v: %v\n", resultDirPath, err)
+        return errors.New("some files weren't processed, check stderr for more information")
+    }
 
-	var okExecutions uint64
+    var okExecutions uint64
 
-	wg := new(sync.WaitGroup)
-	wg.Add(len(paths))
-	for _, path := range paths {
-		go func(p string) {
-			defer wg.Done()
-			if err := processImage(p, resultDirAbsPath); err != nil {
-				fmt.Fprintf(os.Stderr, "Error processing file %v: %v\n", p, err)
-			} else {
-				atomic.AddUint64(&okExecutions, 1)
+    wg := new(sync.WaitGroup)
+    wg.Add(len(paths))
+    for _, path := range paths {
+        go func(p string) {
+            defer wg.Done()
+            if err := processImage(p, resultDirAbsPath); err != nil {
+                fmt.Fprintf(os.Stderr, "Error processing file %v: %v\n", p, err)
+            } else {
+                atomic.AddUint64(&okExecutions, 1)
                 fmt.Printf("%v/%v: Successfuly processed %v\n", atomic.LoadUint64(&okExecutions), len(paths), p)
-			}
-		}(path)
-	}
-	wg.Wait()
+            }
+        }(path)
+    }
+    wg.Wait()
 
-	fmt.Println("Job is done")
-	if okExecutions == uint64(len(paths)) {
-		return nil
-	}
-	return errors.New("Some files weren't processed, check stderr for more information")
+    fmt.Println("Job is done")
+    if okExecutions == uint64(len(paths)) {
+        return nil
+    }
+    return errors.New("some files weren't processed, check stderr for more information")
 }
 
 func processImage(path string, resultDirPath string) error {
-	exists, err := fileExists(path)
-	if err != nil {
-		return err
-	}
-	if !exists {
-		return os.ErrNotExist
-	}
+    exists, err := fileExists(path)
+    if err != nil {
+        return err
+    }
+    if !exists {
+        return os.ErrNotExist
+    }
 
-	inFile, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer inFile.Close()
+    inFile, err := os.Open(path)
+    if err != nil {
+        return err
+    }
+    defer inFile.Close()
 
-	imgData, imgType, err := image.Decode(inFile)
-	if err != nil {
-		return err
-	}
-	r, g, b := splitIntoLayers(&imgData)
+    imgData, imgType, err := image.Decode(inFile)
+    if err != nil {
+        return err
+    }
+    switch imgData.(type) {
+    case *image.Gray, *image.Gray16: {
+    }
+    default:
+        return errors.New("wrong image format")
+    }
+
+    r, g, b := splitIntoLayers(&imgData)
 
     result, err := stackLayers(r, g, b)
     if err != nil {
         return err
     }
-
     filename := filepath.Base(path)
-	resultPath := filepath.Join(resultDirPath, filename)
+    resultPath := filepath.Join(resultDirPath, filename)
     outFile, err := os.Create(resultPath)
     if err != nil {
         return err
@@ -91,59 +97,58 @@ func processImage(path string, resultDirPath string) error {
     if err != nil {
         return err
     }
-
     return nil
 }
 
 func createDir(path string) (string, error) {
-	absPath, err := filepath.Abs(path)
-	if err != nil {
-		return "", err
-	}
-	err = os.MkdirAll(absPath, 0777)
-	return absPath, err
+    absPath, err := filepath.Abs(path)
+    if err != nil {
+        return "", err
+    }
+    err = os.MkdirAll(absPath, 0777)
+    return absPath, err
 }
 
 func fileExists(path string) (bool, error) {
-	if info, err := os.Stat(path); err == nil {
-		return !info.IsDir(), nil
-	} else if errors.Is(err, os.ErrNotExist) {
-		return false, nil
-	} else {
-		return false, err
-	}
+    if info, err := os.Stat(path); err == nil {
+        return !info.IsDir(), nil
+    } else if errors.Is(err, os.ErrNotExist) {
+        return false, nil
+    } else {
+        return false, err
+    }
 }
 
-func splitIntoLayers(img *image.Image) (*image.RGBA64, *image.RGBA64, *image.RGBA64) {
-	bounds := (*img).Bounds()
-	width, height := bounds.Dx(), bounds.Dy()
+func splitIntoLayers(img *image.Image) (*image.Gray16, *image.Gray16, *image.Gray16) {
+    bounds := (*img).Bounds()
+    width, height := bounds.Dx(), bounds.Dy()
 
-	const cutWidthCoeff float64 = 0.05
-	cutWidth := int(float64(width) * cutWidthCoeff)
+    const cutWidthCoeff float64 = 0.05
+    cutWidth := int(float64(width) * cutWidthCoeff)
 
-	const cutHeightCoeff float64 = 0.03
-	cutHeight := int(float64(height / 3) * cutHeightCoeff)
+    const cutHeightCoeff float64 = 0.03
+    cutHeight := int(float64(height / 3) * cutHeightCoeff)
 
     wg := new(sync.WaitGroup)
     wg.Add(3)
 
-	b := image.NewRGBA64(image.Rect(0, 0, width - 2 * cutWidth, height / 3 - 2 * cutHeight))
+    b := image.NewGray16(image.Rect(0, 0, width - 2 * cutWidth, height / 3 - 2 * cutHeight))
     bCopyRect := image.Rect(cutWidth, cutHeight, width - cutWidth, height / 3 - cutHeight)
     go func() {
         draw.Copy(b, image.Pt(0, 0), *img, bCopyRect, draw.Over, nil)
         wg.Done()
     }()
 
-    g := image.NewRGBA64(image.Rect(0, 0, width - 2 * cutWidth, height / 3 - 2 * cutHeight))
+    g := image.NewGray16(image.Rect(0, 0, width - 2 * cutWidth, height / 3 - 2 * cutHeight))
     gCopyRect := image.Rect(cutWidth, height / 3 + cutHeight, width - cutWidth, height / 3 * 2 - cutHeight)
-	go func() {
+    go func() {
         draw.Copy(g, image.Pt(0, 0), *img, gCopyRect, draw.Over, nil)
         wg.Done()
     }()
 
-	r := image.NewRGBA64(image.Rect(0, 0, width - 2 * cutWidth, height / 3 - 2 * cutHeight))
+    r := image.NewGray16(image.Rect(0, 0, width - 2 * cutWidth, height / 3 - 2 * cutHeight))
     rCopyRect := image.Rect(cutWidth, height / 3 * 2 + cutHeight, width - cutWidth, height - cutHeight)
-	go func() {
+    go func() {
         draw.Copy(r, image.Pt(0, 0), *img, rCopyRect, draw.Over, nil)
         wg.Done()
     }()
@@ -153,7 +158,7 @@ func splitIntoLayers(img *image.Image) (*image.RGBA64, *image.RGBA64, *image.RGB
     return r, g, b
 }
 
-func stackLayers(r, g, b *image.RGBA64) (*image.RGBA64, error) {
+func stackLayers(r, g, b *image.Gray16) (*image.RGBA64, error) {
     if (*r).Bounds() != (*g).Bounds() || (*r).Bounds() != (*b).Bounds() || (*g).Bounds() != (*b).Bounds() {
         return nil, errors.New("Layer sizes do not match")
     }
@@ -162,10 +167,10 @@ func stackLayers(r, g, b *image.RGBA64) (*image.RGBA64, error) {
     width, height := result.Bounds().Dx(), result.Bounds().Dy()
     for i := 0; i < width; i++ {
         for j := 0; j < height; j++ {
-            rC, _, _, _ := (*r).At(i, j).RGBA()
-            _, gC, _, _ := (*g).At(i, j).RGBA()
-            _, _, bC, _ := (*b).At(i, j).RGBA()
-            result.Set(i, j, color.RGBA{R: uint8(rC), G: uint8(gC), B: uint8(bC), A: 255})
+            rC := (*r).Gray16At(i, j).Y
+            gC := (*g).Gray16At(i, j).Y
+            bC := (*b).Gray16At(i, j).Y
+            result.Set(i, j, color.RGBA64{R: rC, G: gC, B: bC, A: 255})
         }
     }
     return result, nil
